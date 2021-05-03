435367c4cfd9d95cd6d6c91db2aec040c55dd536
osha7 <ogroetz@gmail.com>
2021-05-03 15:27:22 -0600

    read me and corrected heroku

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -1,5 +1,5 @@
 // // local host url:
-export const API_URL = "http://localhost:3000/api/v1"
+// export const API_URL = "http://localhost:3000/api/v1"
 
 // // hosted website url:
-// export const API_URL = "https://wwc-redux-hooks-demo-backend.herokuapp.com/"
+export const API_URL = "https://wwc-redux-hooks-demo-backend.herokuapp.com/api/v1"
